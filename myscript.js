8956795a74b6367ed9f18c8fdce8114bfa4ecd8a
sarahelshark <sarahelsh183@gmail.com>
2023-12-12 18:27:55 +0100

    feat add
    milestone 0

--- myscript.js
+++ myscript.js
@@ -2,8 +2,7 @@
 Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
 Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 
-MILESTONE 0:
-Creare l’array di oggetti con le informazioni fornite.
+
 
 MILESTONE 1:
 Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
@@ -19,10 +18,44 @@ Organizzare i singoli membri in card/schede
 
 Consigli del giorno: Ragioniamo come sempre a step. Prima la logica in italiano e poi traduciamo in codice.
 E ricordiamoci che console.log() è nostro amico!
-Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
-Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
-Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
-Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
-Scott Estrada	Developer	scott-estrada-developer.jpg
-Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+		
 */
+
+
+//MILESTONE 0:
+
+//Creare l’array di oggetti con le informazioni fornite.
+const posts = [
+    {
+        title:'Wayne Barnett' ,
+        role: 'Founder & CEO',
+        image:"./img/wayne-barnett-founder-ceo.jpg"
+    } ,
+    {
+        title:'Angela Caroll' ,
+        role: 'Chief Editor',
+        image:"./img/angela-caroll-chief-editor.jpg"
+    } ,
+    {
+        title:'Walter Gordon' ,
+        role: 'Office Manager',
+        image:"./img/walter-gordon-office-manager.jpg"
+    } ,
+    {
+        title:'Angela Lopez' ,
+        role: 'Social Media Manager',
+        image:"./img/angela-lopez-social-media-manager.jpg"
+    } ,
+    {
+        title:'Scott Estrada' ,
+        role: 'Developer',
+        image:"./img/scott-estrada-developer.jpg"
+    } ,
+    {
+        title:'Barbara Ramos' ,
+        role: 'Graphic Designer',
+        image:"./img/barbara-ramos-graphic-designer.jpg"
+    } 
+
+]
+console.log(posts);
