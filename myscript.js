1a37465cb2d6a6d890454f18117e26b3cd6e19da
sarahelshark <sarahelsh183@gmail.com>
2023-12-12 18:40:30 +0100

    feat add
     last milestone= bonus 1+2

--- myscript.js
+++ myscript.js
@@ -2,7 +2,8 @@
 Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
 Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 
-
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
 
 MILESTONE 1:
 Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
@@ -16,15 +17,13 @@ Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede
 
-Consigli del giorno: Ragioniamo come sempre a step. Prima la logica in italiano e poi traduciamo in codice.
-E ricordiamoci che console.log() è nostro amico!
+
 		
 */
 
 
-//MILESTONE 0:
-
 //Creare l’array di oggetti con le informazioni fornite.
+//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 const posts = [
     {
         title:'Wayne Barnett' ,
@@ -58,4 +57,32 @@ const posts = [
     } 
 
 ]
+
 console.log(posts);
+
+
+//seleziono luogo dove stampero i miei post e racchiudo in costante
+const postsRowEl = document.querySelector('.posts .row');
+
+
+for(let i = 0; i < posts.length; i++ ){
+    const post = posts[i];
+    //console.log(post); funziona, inserisco markup
+    const postMarkup = `
+    <div class="col">
+     <div class="card">
+            <img class="card-img-top" src="${post.image}" alt="">
+
+            <div class="card-body">
+              <h3> ${post.title} </h3>
+              <p>${post.role}</p>  
+            </div>
+     </div>
+    </div>
+    `
+     //< in questo caso uso template literal `` invece che .crerateElement, che sarebbe troppo laborioso in quanto concatenazione dopo concatenazione ci vorrebbe tanto tempo, dopo aver creato il markup, posso appenderlo alla row
+
+     postsRowEl.insertAdjacentHTML('beforeend', postMarkup);
+     //nn posso usare .insertAdjacentElement, .appendChild xk questo e un template literal
+     //beforeend vuol dire 'inserisci tutto prima di chiudere il tag', e dopo segno cosa devo aggiungere in questo caso il mio template literal con gli elementi di posts
+}
